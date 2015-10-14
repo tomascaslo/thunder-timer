eda9c6cdb2f775ca9f032ab69b39c41de4ce5219
tomascaslo <tomas.caslo90@gmail.com>
2015-10-13 20:36:48 -0500

    add watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
-var gulp = require("gulp");
-var sourcemaps = require("gulp-sourcemaps");
-var babel = require("gulp-babel");
-var concat = require("gulp-concat");
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var babel = require('gulp-babel');
+var concat = require('gulp-concat');
 
-gulp.task("transpile", function () {
-	  return gulp.src("src/**/*.js")
-	    .pipe(sourcemaps.init())
-	    .pipe(babel())
-	    .pipe(concat("all.js"))
-	    .pipe(sourcemaps.write("."))
-	    .pipe(gulp.dest("dist"));
+gulp.task('transpile', function() {
+	return gulp.src('src/**/*.js')
+		.pipe(sourcemaps.init())
+		.pipe(babel())
+		.pipe(concat('all.js'))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist'));
 });
 
+gulp.task('default', function() {
+	gulp.watch(['./index.js', 'src/*.js'], ['transpile']);
+});
